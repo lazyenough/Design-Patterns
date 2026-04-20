ed66891a866a2b7c9361dc92216959ba57f07888
lazyenough <honeyprasad3434@gmail.com>
2026-04-20 16:38:21 +0530

    Added Adaptor Design Pattern

--- Structural Design Patterns/adaptor_design.py
+++ Structural Design Patterns/adaptor_design.py
@@ -1,1 +1,19 @@
 # It allows incompatible interfaces to work together. It acts as a bridge between two unrelated components by translating one interface into another that the client expects.
+
+class EuropeanSocket:
+    def plug_in(self):
+        return "220V power"
+
+class USPlugAdapter:
+    def __init__(self, european_socket: EuropeanSocket):
+        self.socket = european_socket
+    
+    def connect(self):
+        return f"Adapter converts -> {self.socket.plug_in()} from european socket to 110V."
+
+if __name__ == "__main__":
+    european_socket = EuropeanSocket()
+
+    adapter = USPlugAdapter(european_socket)
+
+    print(adapter.connect())
